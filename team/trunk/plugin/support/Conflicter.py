3819c1639592af3aa6645546f7d5e45bc58f4d23
turzap <turzap@d6d51e68-c4c5-48db-96ef-4d935770b557>
2010-04-09 06:51:02 +0000

    More conflicting situations revealed

--- team/trunk/plugin/support/Conflicter.py
+++ team/trunk/plugin/support/Conflicter.py
@@ -7,6 +7,7 @@ from Differ import CDiffer
 from DiffActions import EDiffActions
 import itertools
 from structure import CProjectTreeNode
+from structure import CConnection, CConnectionView
 from Conflict import CConflict
 
 class CConflicter(object):
@@ -107,7 +108,22 @@ class CConflicter(object):
     def __FindConflictsForDataDiff(self, diff, otherDiffer, project):
         result = []
         
+        relatedElements = []
+        if diff.GetAction() != EDiffActions.DELETE:
+            # zisti vsetky elementy vo vztahu
+            if isinstance(diff.GetElement(), CConnection):
+                connectedElements = [project.GetProjectTreeNodeById(diff.GetElement().GetSource().GetId()), project.GetProjectTreeNodeById(diff.GetElement().GetDestination().GetId())]
+                parentsSource = project.GetProjectTreeNodeById(connectedElements[0].GetId()).GetAllParents()
+                parentsDestination = project.GetProjectTreeNodeById(connectedElements[1].GetId()).GetAllParents()
+                relatedElements = connectedElements + parentsSource + parentsDestination
+            else:        
+                relatedElements = project.GetProjectTreeNodeById(diff.GetElement().GetId()).GetAllParents()
+        
+        
         if diff.GetAction() == EDiffActions.MODIFY:
+            
+            
+            
             # ak som upravoval datove zlozky
             for d in otherDiffer.GetDataDiff().get(EDiffActions.MODIFY, []):
                 # prejdi vsetky data diffy z druheho, kde som tiez upravoval
@@ -115,24 +131,39 @@ class CConflicter(object):
                     # ak som upravoval na rovnakom mieste datovu zlozku
                     print 'modyfing same data of elements'
                     result.append(d)
-                    
-            parents = project.GetProjectTreeNodeById(diff.GetElement().GetId()).GetAllParents()
+
+
             
+                
             for d in otherDiffer.GetDataDiff().get(EDiffActions.DELETE, []):
                 # prejdi vsetky data diffy z druheho, ktore som vymazal
-                if d.GetElement() == diff.GetElement() or d.GetElement() in [p.GetObject() for p in parents]:
+                if d.GetElement() == diff.GetElement() or d.GetElement() in [p.GetObject() for p in relatedElements]:
                     # upravoval som vymazany element
                     print 'modifying data of deleted element'
                     result.append(d)
                     
             for d in otherDiffer.GetProjectTreeDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement().GetObject() == diff.GetElement() or d.GetElement() in parents:
+                if d.GetElement().GetObject() == diff.GetElement() or d.GetElement() in relatedElements:
                     # upravoval som vymazany element
                     print 'modifying data of deleted element'
                     result.append(d)
+                    
+            
         
         elif diff.GetAction() == EDiffActions.INSERT:
-            pass
+            for d in otherDiffer.GetProjectTreeDiff().get(EDiffActions.DELETE, []):
+                # prejdi vsetky vymazane elementy
+                if d.GetElement() in relatedElements:
+                    # pozri ci je na ceste k vlozenemu elementu
+                    print 'insert element under deleted element'
+                    result.append(d)
+                    
+            for d in otherDiffer.GetDataDiff().get(EDiffActions.DELETE, []):
+                # prejdi vsetky vymazane elementy
+                if d.GetElement() in [p.GetObject() for p in relatedElements]:
+                    # pozri ci je na ceste k vlozenemu elementu
+                    print 'insert element under deleted element'
+                    result.append(d)
         
         elif diff.GetAction() == EDiffActions.DELETE:
             pass
@@ -219,16 +250,39 @@ class CConflicter(object):
     
     def __FindConflictsForVisualDiff(self, diff, otherDiffer, project):
         result = []
+        # zisti vsetky elementy vo vztahu
+        print diff
+        
+        relatedElements = []
+        relatedViews = []
+        if diff.GetAction() != EDiffActions.DELETE:
+            if isinstance(diff.GetElement(), CConnectionView):
+                connectedElements = [project.GetProjectTreeNodeById(diff.GetElement().GetObject().GetSource().GetId()), project.GetProjectTreeNodeById(diff.GetElement().GetObject().GetDestination().GetId())]
+                parentsSource = project.GetProjectTreeNodeById(connectedElements[0].GetId()).GetAllParents()
+                parentsDestination = project.GetProjectTreeNodeById(connectedElements[1].GetId()).GetAllParents()
+                relatedElements = connectedElements + parentsSource + parentsDestination
+                relatedViews = [diff.GetElement().GetParentDiagram().GetViewById(connectedElements[0].GetId()), diff.GetElement().GetParentDiagram().GetViewById(connectedElements[1].GetId())]
+                
+            else:        
+                relatedElements = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
+        
+        
+        
         if diff.GetAction() == EDiffActions.INSERT:
-            parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
+            #parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
             for d in otherDiffer.GetProjectTreeDiff().get(EDiffActions.DELETE,[]):
-                if d.GetElement().GetObject() == diff.GetElement.GetParentDiagram() or d.GetElement() in parents:
+                if d.GetElement().GetObject() == diff.GetElement.GetParentDiagram() or d.GetElement() in relatedElements:
                     print 'insert view under deleted diagram'
                     result.append(d)
             
             for d in otherDiffer.GetDataDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement() == diff.GetElement().GetParentDiagram() or d.GetElement() in [p.GetObject() for p in parents]:
+                if d.GetElement() == diff.GetElement().GetParentDiagram() or d.GetElement() in [p.GetObject() for p in relatedElements]:
                     print 'insert view under deleted diagram'
+                    result.append(d)
+                    
+            for d in otherDiffer.GetVisualDiff().get(EDiffActions.DELETE, []):
+                if d.GetElement() in relatedViews:
+                    print 'deleted view for inserted connection'
                     result.append(d)
             
         elif diff.GetAction() == EDiffActions.DELETE:
@@ -237,19 +291,19 @@ class CConflicter(object):
         elif diff.GetAction() == EDiffActions.MODIFY:
             
             for d in otherDiffer.GetVisualDiff().get(EDiffActions.DELETE,[]):
-                if d.GetElement() == diff.GetElement():
+                if d.GetElement() == diff.GetElement() or d.GetElement() in relatedViews:
                     print 'modifying deleted view'
                     result.append(d)
                     
                     
-            parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
+            #parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
             for d in otherDiffer.GetProjectTreeDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement().GetObject() == diff.GetElement().GetParentDiagram() or d.GetElement() in parents:
+                if d.GetElement().GetObject() == diff.GetElement().GetParentDiagram() or d.GetElement() in relatedElements:
                     print 'modifying view in deleted diagram'
                     result.append(d)
                     
             for d in otherDiffer.GetDataDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement() == diff.GetElement().GetParentDiagram()  or d.GetElement() in [p.GetObject() for p in parents]:
+                if d.GetElement() == diff.GetElement().GetParentDiagram()  or d.GetElement() in [p.GetObject() for p in relatedElements]:
                     print 'modifying view in deleted diagram'
                     result.append(d)
                     
@@ -257,7 +311,7 @@ class CConflicter(object):
                 if d.GetDataPath() == diff.GetDataPath():
                     print 'modifying same visual data of views'
                     result.append(d)
-            
+                    
             
         elif diff.GetAction() == EDiffActions.ORDER_CHANGE:
             for d in otherDiffer.GetVisualDiff().get(EDiffActions.ORDER_CHANGE, []):
@@ -267,7 +321,7 @@ class CConflicter(object):
                         result.append(d)
             
             
-            parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
+            #parents = project.GetProjectTreeNodeById(diff.GetElement().GetParentDiagram().GetId()).GetAllParents()
                         
             for d in otherDiffer.GetVisualDiff().get(EDiffActions.DELETE,[]):
                 if d.GetElement() == diff.GetElement():
@@ -275,12 +329,12 @@ class CConflicter(object):
                     result.append(d)
             
             for d in otherDiffer.GetProjectTreeDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement().GetObject() == diff.GetElement().GetParentDiagram() or d.GetElement() in parents:
+                if d.GetElement().GetObject() == diff.GetElement().GetParentDiagram() or d.GetElement() in relatedElements:
                     print 'changing order of view in deleted diagram'
                     result.append(d)
                     
             for d in otherDiffer.GetDataDiff().get(EDiffActions.DELETE, []):
-                if d.GetElement() == diff.GetElement().GetParentDiagram() or d.GetElement() in [p.GetObject() for p in parents]:
+                if d.GetElement() == diff.GetElement().GetParentDiagram() or d.GetElement() in [p.GetObject() for p in relatedElements]:
                     print 'changing order of view in deleted diagram'
                     result.append(d)
         return result
