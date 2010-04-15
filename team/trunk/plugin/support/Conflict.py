cd042547eb10635e947bc012d1c367ef827f7a38
turzap <turzap@d6d51e68-c4c5-48db-96ef-4d935770b557>
2010-04-15 22:58:58 +0000

    Finding related conflicts in conflicts resolve dialog

--- team/trunk/plugin/support/Conflict.py
+++ team/trunk/plugin/support/Conflict.py
@@ -10,13 +10,17 @@ class CConflict(object):
     '''
 
 
-    def __init__(self, baseWorkDiff, baseNewDiff, text = ''):
+    def __init__(self, baseWorkDiff, baseNewDiff, text = '', relatedObjects = None):
         '''
         Constructor
         '''
         self.__baseWorkDiff = baseWorkDiff
         self.__baseNewDiff = baseNewDiff
         self.__text = text
+        if relatedObjects is None:
+            self.__relatedObjects = []
+        else:
+            self.__relatedObjects = relatedObjects
         
         
     def GetBaseWorkDiff(self):
@@ -24,6 +28,9 @@ class CConflict(object):
     
     def GetBaseNewDiff(self):
         return self.__baseNewDiff
+    
+    def GetRelatedObjects(self):
+        return self.__relatedObjects
     
     def GetText(self):
         return self.__text
