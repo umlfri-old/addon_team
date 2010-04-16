0684ffc2aedf5653dbe2c94480da71c824b957ac
turzap <turzap@d6d51e68-c4c5-48db-96ef-4d935770b557>
2010-04-16 09:28:54 +0000

    missing exceptions added

---
+++ team/trunk/plugin/teamExceptions/AuthorizationError.py
@@ -0,0 +1,14 @@
+'''
+Created on 14.4.2010
+
+@author: Peterko
+'''
+
+class AuthorizationError(Exception):
+    '''
+    classdocs
+    '''
+
+
+    pass
+        
---
+++ team/trunk/plugin/teamExceptions/__init__.py
@@ -0,0 +1,1 @@
+from AuthorizationError import AuthorizationError
